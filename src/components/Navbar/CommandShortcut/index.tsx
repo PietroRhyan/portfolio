41a2fbd49eb25e1d149ac6ac075fa18579ace38b
Pietro Rhyan <pietroqjg@gmail.com>
2023-10-24 13:31:45 -0300

    fix: some colors in navbar

--- src/components/Navbar/CommandShortcut/index.tsx
+++ src/components/Navbar/CommandShortcut/index.tsx
@@ -2,7 +2,7 @@ import { FiCommand } from 'react-icons/fi'
 
 export function CommandShortcut() {
   return (
-    <button className="p-2 rounded-full cursor-pointer hover:bg-lightgray text-black transition-colors duration-200">
+    <button className="p-2 rounded-full cursor-pointer hover:bg-lightgray text-text-gray hover:text-black transition-colors duration-200">
       <FiCommand size={24} />
     </button>
   )
--- src/components/Navbar/ThemeSwitcher/index.tsx
+++ src/components/Navbar/ThemeSwitcher/index.tsx
@@ -2,7 +2,7 @@ import { FiMoon } from 'react-icons/fi'
 
 export function ThemeSwitcher() {
   return (
-    <button className="p-2 rounded-full cursor-pointer hover:bg-lightgray text-black hover:text-black transition-colors duration-200">
+    <button className="p-2 rounded-full cursor-pointer hover:bg-lightgray text-text-gray hover:text-black transition-colors duration-200">
       <FiMoon size={24} />
     </button>
   )
